d0f9c6e5586446cc3c04af1a80f3c7def0449fdd
Rui Lopes <rgl@ruilopes.com>
2022-07-27 21:26:04 +0100

    upgrade to chocolatey 1.1.0

--- provision-chocolatey.ps1
+++ provision-chocolatey.ps1
@@ -1,3 +1,3 @@
-$env:chocolateyVersion = '0.11.3'
+$env:chocolateyVersion = '1.1.0'
 
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')
